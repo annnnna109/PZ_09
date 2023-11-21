20849b7cc83026996ff6769964b68db5771033d1
annnnna109 <144040921+annnnna109@users.noreply.github.com>
2023-11-21 21:57:45 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿namespace PZ_09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                static void Main(string[] args)
+                {
+                    Console.WriteLine("Введите произвольную строку:");
+                    string input = Console.ReadLine();
+
+                    int uppercaseCount = 0;
+                    int lowercaseCount = 0;
+                    int whitespaceCount = 0;
+                    int punctuationCount = 0;
+
+                    foreach (char ch in input)
+                    {
+                        if (char.IsUpper(ch))
+                        {
+                            uppercaseCount++;
+                        }
+                        else if (char.IsLower(ch))
+                        {
+                            lowercaseCount++;
+                        }
+                        else if (char.IsWhiteSpace(ch))
+                        {
+                            whitespaceCount++;
+                        }
+                        else if (char.IsPunctuation(ch))
+                        {
+                            punctuationCount++;
+                        }
+                    }
+
+                    int totalCount = uppercaseCount + lowercaseCount + whitespaceCount;
+
+                    double punctuationPercentage = (double)punctuationCount / totalCount * 100;
+
+                    Console.WriteLine("Заглавных букв: " + uppercaseCount);
+                    Console.WriteLine("Строчных букв: " + lowercaseCount);
+                    Console.WriteLine("Количество пробельных символов: " + whitespaceCount);
+                    Console.WriteLine("Процент символов пунктуации: " + punctuationPercentage.ToString("0") + "%");
+                }
+            }
+
+        }
+    }
+}
